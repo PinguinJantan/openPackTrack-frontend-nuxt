fb6902d37dcfb26be63796b69826f558530a7627
Diky Arga <dikyarga.id@gmail.com>
2018-02-12 00:49:01 +0700

    initial commit :tada:

---
+++ components/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img src="" alt="">  
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <logo/>
+      </div>
+      <v-card>
+        <v-card-title class="headline">Welcome to the PackTrack</v-card-title>
+        <v-card-text>
+          <p>Packing and Tracking System</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  }
+}
+</script>
