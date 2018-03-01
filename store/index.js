c05b94afdd4396b4d5dfdebfd9c62fb7a9e5a72f
Diky Arga <dikyarga.id@gmail.com>
2018-03-02 01:01:09 +0700

    create vuex store

---
+++ store/index.js
@@ -0,0 +1,38 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      token: null,
+    },
+    mutations: {
+      setToken(state, token) {
+        state.token = token;
+      },
+      clearToken(state) {
+        state.token = null;
+      }
+    },
+    actions: {
+      initAuth(vuexContext, req) {
+        let token;
+
+        if (req) {
+          if (!req.headers.cookie) {
+            return;
+          }
+        }
+      },
+      login(vuexContext, authData) {
+        this.$axios.$get('/items').then(result => {
+          console.log('the result : ', result)
+        }).catch(err => {
+          console.log('the errors : ', err)
+        })
+        console.log('trying to login')
+      }
+    }
+  })
+}
+
+export default createStore;
