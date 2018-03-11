c38c708a7d8eb0cdd4b9e6ad5fd4a60c684c4e05
Diky Arga <dikyarga@192.168.1.6>
2018-03-11 13:57:52 +0700

    move fans logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo/>
-      </div>
       <v-card>
         <v-card-title class="headline">Welcome to the PackTrack</v-card-title>
         <v-card-text>
           <p>Packing and Tracking System</p>
+          <div class="text-xs-center">
+            <logo/>
+          </div>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
