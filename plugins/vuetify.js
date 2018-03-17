3553a28f26a9b5b74ae3e547fc7668bc59560949
wkwksama <mailpakgasing@gmail.com>
2018-03-17 19:22:00 +0700

    set theme color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    primary: '#0fb9b1', // a color that is not in the material colors palette
+    accent: '#778ca3',
+    secondary: '#4b6584',
+    info: '#4b7bec',
+    warning: '#fed330',
+    error: '#fc5c65',
+    success: '#26de81'
   }
 })
