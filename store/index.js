2366b65fd6bf8d769ecf435e210aef21fc155100
dikyarga <dikyarga.id@gmail.com>
2018-08-05 18:00:40 +0700

    remove unnecessary auth related stuff

--- store/index.js
+++ store/index.js
@@ -1,11 +1,8 @@
 import Vuex from "vuex";
 
-import authUtil from "~/utils/auth";
-
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      token: null,
       notification: {
         color: 'error',
         active: false,
@@ -13,12 +10,6 @@ const createStore = () => {
       }
     },
     mutations: {
-      setToken(state, token) {
-        state.token = token;
-      },
-      clearToken(state) {
-        state.token = null;
-      },
       SET_NOTIFICATION(state, payload) {
         state.notification.active = !state.notification.active
         state.notification.message = payload.message
@@ -26,59 +17,11 @@ const createStore = () => {
       }
     },
     actions: {
-      nuxtServerInit(vuexContext, { req }) {
-        vuexContext.dispatch("initAuth", req);
-      },
-      initAuth(vuexContext, req) {
-        let token;
-
-        if (req) {
-          if (!req.headers.cookie) {
-            return;
-          }
-
-          const jwtCookie = req.headers.cookie
-            .split(";")
-            .find(c => c.trim().startsWith("jwt="));
-
-          if (!jwtCookie) {
-            return;
-          }
-
-          token = jwtCookie.split("=")[1];
-        } else {
-          token = localStorage.getItem("token");
-        }
-
-        vuexContext.commit("setToken", token);
-      },
-      login(vuexContext, authData) {
-        this.$axios
-          .$post("/auth/login", authData)
-          .then(result => {
-            if (result.success) {
-              vuexContext.commit("setToken", result.user.token);
-              authUtil.setToken(result.user.token);
-            } else {
-              vuexContext.dispatch('notify', { type: 'error', message: result.message})
-            }
-          })
-          .catch(err => {
-            vuexContext.dispatch('notify', { type: 'error', message: result.message})
-          });
-      },
-      logout(vuexContext) {
-        vuexContext.commit("clearToken");
-        if (process.client) {
-          authUtil.unsetToken();
-        }
-      },
       notify(vuexCtx, payload) {
         vuexCtx.commit('SET_NOTIFICATION', payload)
       }
     },
     getters: {
-      isLoggedIn: state => state.token !== null,
       alert: state => state.alert,
     }
   });
