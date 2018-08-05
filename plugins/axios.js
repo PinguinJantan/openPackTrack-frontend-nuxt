61e6eba54b42d1ba56374064ef87e7a7a7bfe5df
dikyarga <dikyarga.id@gmail.com>
2018-08-05 18:36:45 +0700

    remove axios config, because already configured by auth module

--- plugins/axios.js
+++
@@ -1,7 +0,0 @@
-export default function ({ $axios, store, $auth }) {
-  $axios.onRequest(config => {
-    if (store.state.token) {
-      config.headers.common['Authorization'] = `Bearer ${store.state.token}`
-    }
-  })
-}
