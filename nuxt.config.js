e0683f1e84460e3bec5775c01671d2af6530e6bf
dikyarga <dikyarga.id@gmail.com>
2018-08-07 00:51:40 +0700

    retrieve user detail right after sign-in

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ module.exports = {
         endpoints: {
           login: { url: '/auth/login', method: 'post', propertyName: 'user.token' },
           logout:false, 
-          user: false,
+          user: { url: '/user/me', method: 'get', propertyName: 'user'},
         },
         tokenRequired: true,
       }
