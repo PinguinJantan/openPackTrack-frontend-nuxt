b741601cd8ca890ceb6ff2a7800c0c3dcfae4dc0
dikyarga <dikyarga.id@gmail.com>
2018-08-09 00:23:20 +0700

    move item form into a component for re-usability

---
+++ components/items/ItemForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <form>
+      <div style="display: flex; margin-bottom: 8px">
+        <v-flex text-xs-left>
+          <vue-multiselect
+            :options="skuOptions"
+            v-model="sku"
+            label="name"
+            track-by="name"
+            placeholder="SKU"
+            :allow-empty="false"
+            tag-position="bottom"
+            data-vv-as="SKU"
+            data-vv-name="sku"
+            v-validate="'required'">
+            <template slot="singleLabel" slot-scope="props">
+              {{ props.option.code }} ({{ props.option.name }})
+            </template>
+            <template slot="option" slot-scope="props">
+              {{ props.option.code }} ({{ props.option.name }})
+            </template>
+          </vue-multiselect>
+          <div class="field-error v-messages error--text">{{ errors.has('sku') ? errors.first('sku') : '' }}</div>
+        </v-flex>
+        <v-btn color="green" large @click="showAddSku = true">
+          <v-icon color="white">add</v-icon>
+        </v-btn>
+      </div>
+      <v-text-field
+        v-model="code"
+        label="Code"
+        :error-messages="errors.collect('code')"
+        v-validate="'required'"
+        data-vv-name="code"
+        required
+        outline
+      />
+      <v-flex text-xs-left>
+        <vue-multiselect
+          :options="sizeOptions"
+          v-model="size"
+          label="name"
+          track-by="name"
+          placeholder="Ukuran"
+          :allow-empty="false"
+          tag-position="bottom"
+          @tag="addSizeTag"
+          :taggable="true"
+          v-validate="'required'"
+          data-vv-as="Ukuran"
+          data-vv-name="ukuran"/>
+        <div class="field-error v-messages error--text">{{ errors.has('ukuran') ? errors.first('ukuran') : '' }}</div>
+      </v-flex>
+      <v-btn color="primary" @click="create">Buat Item Baru</v-btn>
+    </form>
+    <add-sku v-model="showAddSku" @success="onSuccessAddSku"/>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex';
+import AddSku from '@/components/items/addSku';
+
+export default {
+  name: 'ItemForm',
+  $_veeValidate: {
+    validator: 'new',
+  },
+  components: {
+    AddSku,
+  },
+  data() {
+    return {
+      code: '',
+      sizeOptions: [],
+      size: '',
+      skuOptions: [],
+      sku: '',
+      showAddSku: false,
+    };
+  },
+  mounted() {
+    this.fetchOptions('size');
+    this.fetchOptions('sku');
+  },
+  methods: {
+    ...mapActions(['notify']),
+    create() {
+      this.$validator.validateAll().then(isFormValid => {
+        if (isFormValid) {
+          this.$axios
+            .$post('/api/item/create', {
+              code: this.code,
+              size: this.size.name,
+              skuId: this.sku.id,
+            })
+            .then(res => {
+              if (res.success) {
+                this.$router.push({
+                  name: 'items',
+                });
+                return;
+              }
+              this.notify({ type: 'info', message: res.message });
+            })
+            .catch(err => {
+              this.notify({ type: 'error', message: err.message });
+            });
+        }
+      });
+    },
+    fetchOptions(option) {
+      this.$axios
+        .$get(`/api/${option}/list`)
+        .then(res => {
+          if (res.success) {
+            this[`${option}Options`] = res[`${option}s`];
+            return;
+          }
+
+          this.notify({ type: 'error', message: res.message });
+        })
+        .catch(err => {
+          this.notify({ type: 'error', message: err.message });
+        });
+    },
+    addSizeTag(newTag) {
+      this.sizeOptions.push({ name: newTag });
+      this.size = { name: newTag };
+    },
+    addSkuTag(newTag) {
+      this.sizeOptions.push(newTag);
+      this.size = newTag;
+    },
+    onSuccessAddSku(data) {
+      this.sku = data.sku;
+      this.fetchOptions('sku');
+    },
+  },
+};
+</script>
--- pages/items/create.vue
+++ pages/items/create.vue
@@ -6,145 +6,19 @@
             text-xs-center
             mt-5>
       <h3>Buat Item Baru</h3>
-      <form>
-        <div style="display: flex; margin-bottom: 8px">
-          <v-flex text-xs-left>
-            <vue-multiselect
-              :options="skuOptions"
-              v-model="sku"
-              label="name"
-              track-by="name"
-              placeholder="SKU"
-              :allow-empty="false"
-              tag-position="bottom"
-              data-vv-as="SKU"
-              data-vv-name="sku"
-              v-validate="'required'">
-              <template slot="singleLabel" slot-scope="props">
-                {{ props.option.code }} ({{ props.option.name }})
-              </template>
-              <template slot="option" slot-scope="props">
-                {{ props.option.code }} ({{ props.option.name }})
-              </template>
-            </vue-multiselect>
-            <div class="field-error v-messages error--text">{{ errors.has('sku') ? errors.first('sku') : '' }}</div>
-          </v-flex>
-          <v-btn color="green" large @click="showSkuModal">
-            <v-icon color="white">add</v-icon>
-          </v-btn>
-        </div>
-        <v-text-field
-          v-model="code"
-          label="Code"
-          :error-messages="errors.collect('code')"
-          v-validate="'required'"
-          data-vv-name="code"
-          required
-          outline
-        />
-        <v-flex text-xs-left>
-          <vue-multiselect
-            :options="sizeOptions"
-            v-model="size"
-            label="name"
-            track-by="name"
-            placeholder="Ukuran"
-            :allow-empty="false"
-            tag-position="bottom"
-            @tag="addSizeTag"
-            :taggable="true"
-            v-validate="'required'"
-            data-vv-as="Ukuran"
-            data-vv-name="ukuran"/>
-          <div class="field-error v-messages error--text">{{ errors.has('ukuran') ? errors.first('ukuran') : '' }}</div>
-        </v-flex>
-        <v-btn color="primary" @click="create">Buat Item Baru</v-btn>
-      </form>
+      <item-form/>
     </v-flex>
-    <add-sku v-model="showAddSku" @success="onSuccessAddSku"/>
   </v-layout>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-import addSku from '@/components/items/addSku.vue';
+import ItemForm from '@/components/items/ItemForm';
 
 export default {
   name: 'CreateItem',
-  $_veeValidate: {
-    validator: 'new',
-  },
-  components: { addSku },
+  components: { ItemForm },
   data() {
-    return {
-      code: '',
-      sizeOptions: [],
-      size: '',
-      skuOptions: [],
-      sku: '',
-      showAddSku: false,
-    };
-  },
-  mounted() {
-    this.fetchOptions('size');
-    this.fetchOptions('sku');
-  },
-  methods: {
-    ...mapActions(['notify']),
-    showSkuModal() {
-      this.showAddSku = true;
-    },
-    create() {
-      this.$validator.validateAll().then(isFormValid => {
-        if (isFormValid) {
-          this.$axios
-            .$post('/api/item/create', {
-              code: this.code,
-              size: this.size.name,
-              skuId: this.sku.id,
-            })
-            .then(res => {
-              if (res.success) {
-                this.$router.push({
-                  name: 'items',
-                });
-                return;
-              }
-              this.notify({ type: 'info', message: res.message });
-            })
-            .catch(err => {
-              this.notify({ type: 'error', message: err.message });
-            });
-        }
-      });
-    },
-    fetchOptions(option) {
-      this.$axios
-        .$get(`/api/${option}/list`)
-        .then(res => {
-          if (res.success) {
-            this[`${option}Options`] = res[`${option}s`];
-            return;
-          }
-
-          this.notify({ type: 'error', message: res.message });
-        })
-        .catch(err => {
-          this.notify({ type: 'error', message: err.message });
-        });
-    },
-    addSizeTag(newTag) {
-      this.sizeOptions.push({ name: newTag });
-      this.size = { name: newTag };
-    },
-    addSkuTag(newTag) {
-      this.sizeOptions.push(newTag);
-      this.size = newTag;
-    },
-    onSuccessAddSku(data) {
-      this.sku = data.sku;
-      this.fetchOptions('sku');
-    },
+    return {};
   },
 };
 </script>
