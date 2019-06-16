e95f95779faa1ef1bd0f54a72e600cd25ecd8776
dikyarga <dikyarga.id@gmail.com>
2019-06-16 21:31:54 +0700

    fetch role resources

--- store/index.js
+++ store/index.js
@@ -1,11 +1,35 @@
 export const state = () => ({
+  roleResources: []
 })
 
 export const mutations = {
+  SET_ROLE_RESOURCES (state, resources) {
+    state.roleResources = resources
+  }
 }
 
 export const actions = {
+  async nuxtServerInit ({ commit }, ctx) {
+    try {
+      // @TODO
+      // const result = await ctx.$axios.get(`/api/user/role/admin/resource`)
+      const result = {
+        data: [
+          {
+            name: 'item'
+          },
+          {
+            name: 'user'
+          }
+        ]
+      }
+      commit('SET_ROLE_RESOURCES', result.data)
+    } catch (error) {
+      console.log('error :', error.message)
+    }
+  }
 }
 
 export const getters = {
+  roleResources: state => state.roleResources
 }
